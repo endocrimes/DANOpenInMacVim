01f021446c097f3a0ec8e80cd43725739eaabfa7
Daniel Tomlinson <Dan@Tomlinson.io>
2015-10-07 17:02:28 +0100

    Initial Commit

---
+++ DANOpenInMacVim/DANOpenInMacVim.h
@@ -0,0 +1,21 @@
+//
+//  DANOpenInMacVim.h
+//  DANOpenInMacVim
+//
+//  Created by Daniel Tomlinson on 07/10/2015.
+//  Copyright © 2015 Rocket Apps. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@class DANOpenInMacVim;
+
+static DANOpenInMacVim *sharedPlugin;
+
+@interface DANOpenInMacVim : NSObject
+
++ (instancetype)sharedPlugin;
+- (id)initWithBundle:(NSBundle *)plugin;
+
+@property (nonatomic, strong, readonly) NSBundle* bundle;
+@end
---
+++ DANOpenInMacVim/NSObject_Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSObject_Extension.h
+//  DANOpenInMacVim
+//
+//  Created by Daniel Tomlinson on 07/10/2015.
+//  Copyright © 2015 Rocket Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Xcode_Plugin_Template_Extension)
+
++ (void)pluginDidLoad:(NSBundle *)plugin;
+
+
+@end
