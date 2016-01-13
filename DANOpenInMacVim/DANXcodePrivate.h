1f1ef9b3f8f21115eab643b276442f9eecef0b4a
Danielle Lancashire <dani@builds.terrible.systems>
2016-01-13 13:33:22 +0000

    Update README.md

--- DANOpenInMacVim/DANXcodePrivate.h
+++ DANOpenInMacVim/DANXcodePrivate.h
@@ -2,7 +2,7 @@
 //  DANXcodePrivate.h
 //  DANOpenInMacVim
 //
-//  Created by  Danielle Lancashireon 07/10/2015.
+//  Created by Danielle Lancashire on 07/10/2015.
 //  Copyright © 2015 Rocket Apps. All rights reserved.
 //
 
